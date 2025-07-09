87fcb672af6a95c9f9a71de7ff3b6cfb38beda48
Jayden1Joe <jojojo69988@naver.com>
2025-07-10 02:40:57 +0900

    UI(Seat page)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_train_app/pages/seat/seat_page.dart';
 import 'package:flutter_train_app/pages/station_list/station_list_page.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const StationListPage(),
+      home: const SeatPage(),
     );
   }
 }
--- lib/pages/seat/seat_page.dart
+++ lib/pages/seat/seat_page.dart
@@ -8,8 +8,142 @@ class SeatPage extends StatefulWidget {
 }
 
 class _SeatPageState extends State<SeatPage> {
+  bool isSelected = false;
+  int? selectedRow;
+  String? selectedCol;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('좌석 선택'),
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back_ios)),
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                '수서',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.purple,
+                ),
+              ),
+              Icon(Icons.arrow_circle_right_outlined, size: 30),
+              Text(
+                '부산',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.purple,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 24,
+                height: 24,
+                decoration: BoxDecoration(
+                  color: Colors.purple,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              SizedBox(width: 4),
+              Text('선택됨'),
+              SizedBox(width: 20),
+              Container(
+                width: 24,
+                height: 24,
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              SizedBox(width: 4),
+              Text('선택안됨'),
+            ],
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: ListView(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      labelBox('A', alignment: Alignment.bottomCenter),
+                      labelBox('B', alignment: Alignment.bottomCenter),
+                      SizedBox(width: 54, height: 58),
+                      labelBox('C', alignment: Alignment.bottomCenter),
+                      labelBox('D', alignment: Alignment.bottomCenter),
+                    ],
+                  ),
+                  for (int i = 1; i <= 20; i++) seatRow(i),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: ElevatedButton(
+        onPressed: () {},
+        child: Text('data'),
+      ),
+    );
+  }
+
+  SizedBox labelBox(String text, {Alignment alignment = Alignment.center}) {
+    return SizedBox(
+      width: 54,
+      height: 58,
+      child: Align(
+        alignment: alignment,
+        child: Text(text, style: TextStyle(fontSize: 18)),
+      ),
+    );
+  }
+
+  Row seatRow(int rowNum) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        seat(rowNum, 'A'),
+        seat(rowNum, 'B'),
+        SizedBox(width: 54, height: 58),
+        seat(rowNum, 'C'),
+        seat(rowNum, 'D'),
+      ],
+    );
+  }
+
+  Widget seat(int rowNum, String col) {
+    return col != ''
+        ? Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 2, vertical: 4),
+            child: GestureDetector(
+              onTap: () {
+                selectedCol = col;
+                selectedRow = rowNum;
+                setState(() {});
+              },
+              child: Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: selectedCol == col && selectedRow == rowNum
+                      ? Colors.purple
+                      : Colors.grey[300],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
+          )
+        : labelBox(rowNum.toString());
   }
 }

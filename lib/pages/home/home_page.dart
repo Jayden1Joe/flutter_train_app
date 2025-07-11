9e47237f744670b549abd615eb5779cf05959eea
Jayden1Joe <jojojo69988@naver.com>
2025-07-11 12:06:19 +0900

    Refactor(Select sation widget component)

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_train_app/pages/home/widgets/select_station.dart';
 import 'package:flutter_train_app/pages/seat/seat_page.dart';
 import 'package:flutter_train_app/pages/station_list/station_list_page.dart';
 
@@ -76,42 +77,6 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
-      ),
-    );
-  }
-
-  GestureDetector selectStation(
-    BuildContext context,
-    String title,
-    String station,
-    void Function(String) onSelected,
-  ) {
-    return GestureDetector(
-      onTap: () async {
-        final selected = await Navigator.push<String>(
-          context,
-          MaterialPageRoute(builder: (_) => StationListPage(title)),
-        );
-        if (selected != null) {
-          onSelected(selected);
-        }
-      },
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            title,
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.grey,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Text(
-            station.isEmpty ? '선택' : station,
-            style: TextStyle(fontSize: 40),
-          ),
-        ],
       ),
     );
   }
---
+++ lib/pages/home/widgets/select_station.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_train_app/pages/station_list/station_list_page.dart';
+
+GestureDetector selectStation(
+  BuildContext context,
+  String title,
+  String station,
+  void Function(String) onSelected,
+) {
+  return GestureDetector(
+    onTap: () async {
+      final selected = await Navigator.push<String>(
+        context,
+        MaterialPageRoute(builder: (_) => StationListPage(title)),
+      );
+      if (selected != null) {
+        onSelected(selected);
+      }
+    },
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.grey,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(station.isEmpty ? '선택' : station, style: TextStyle(fontSize: 40)),
+      ],
+    ),
+  );
+}

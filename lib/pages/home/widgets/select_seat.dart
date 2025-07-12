a4a7fdcb2530ef3a7e86f3581d75081d3f076708
Jayden1Joe <jojojo69988@naver.com>
2025-07-13 01:30:46 +0900

    Exception(Home page station not selected)

--- lib/pages/home/widgets/select_seat.dart
+++ lib/pages/home/widgets/select_seat.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_train_app/pages/seat/seat_page.dart';
 
@@ -9,6 +10,14 @@ class SelectSeat extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String alertMessage = '';
+    if (departure.isEmpty && arrival.isEmpty) {
+      alertMessage = '출발역과 도착역을 선택해주세요';
+    } else if (departure.isEmpty) {
+      alertMessage = '출발역을 선택해주세요';
+    } else if (arrival.isEmpty) {
+      alertMessage = '도착역을 선택해주세요';
+    }
     return ElevatedButton(
       onPressed: () {
         if (departure.isNotEmpty && arrival.isNotEmpty) {
@@ -17,6 +26,23 @@ class SelectSeat extends StatelessWidget {
             MaterialPageRoute(
               builder: (_) => SeatPage(departure, arrival),
             ), //도착역과 출발역의 정보를 가지고 좌석 예매 페이지로 전환
+          );
+        } else {
+          showCupertinoDialog(
+            context: context,
+            builder: (context) {
+              return CupertinoAlertDialog(
+                title: Text(alertMessage),
+                actions: [
+                  CupertinoDialogAction(
+                    child: Text('확인'),
+                    onPressed: () {
+                      Navigator.pop(context); // pop을 두번 실행해서 초기 홈페이지로 돌아감.
+                    },
+                  ),
+                ],
+              );
+            },
           );
         }
       },

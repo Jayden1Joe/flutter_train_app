a70b61768ab8cfd96cc90265bb9898a76acb4bd4
Jayden1Joe <jojojo69988@naver.com>
2025-07-14 13:41:04 +0900

    Feature(Button widget)

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
 
+class button extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.purple,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        minimumSize: Size(double.infinity, 58),
+      ),
+      child: Text(
+        '좌석 선택',
+        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
